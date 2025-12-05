9d8cc0cacb4f5b17bb82276d1901ef6be3794cfa
ceetek1 <charles.chukwurah@pau.edu.ng>
2025-12-05 19:38:43 +0100

    My prectice for reading and writing

---
+++ reading_and_writing_files/src/main.rs
@@ -0,0 +1,21 @@
+//use std::io::Write;
+
+//fn main(){
+    //To write into a file in rust it is sha tideous but sha pussible tho 
+
+// In this line i want to like declare a variable to be able to input into the file 
+// It is not really necessary sha i can still write into the file its still sha the same thing 
+
+//let written_data = "Omo i am sha writing into this file now i want to experiment this to see wether it works \n na to sha get 5.0 be the goal tho and to make my parents really proud";
+//let  mut file = std::fs::File::create("my data.txt").expect("Failed to open file");
+//file.write_all("I LOVE RUST".as_bytes()).expect("write failed");
+//println!("I don write am abeg 🥳😭");
+
+//}
+use std::io::Read;
+fn main(){
+    let mut file = std::fs::File::open("my data.txt").expect("failed to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    print!("{}",contents);
+}
---
+++ reading_and_writing_files/target/debug/deps/reading_and_writing_files-682bb793d8abcd66.d
@@ -0,0 +1,5 @@
+/Users/chukwurahcharles/Desktop/Programming tinz/MY RUST PROJECTS/reading_and_writing_files/target/debug/deps/reading_and_writing_files-682bb793d8abcd66.d: src/main.rs
+
+/Users/chukwurahcharles/Desktop/Programming tinz/MY RUST PROJECTS/reading_and_writing_files/target/debug/deps/reading_and_writing_files-682bb793d8abcd66: src/main.rs
+
+src/main.rs:

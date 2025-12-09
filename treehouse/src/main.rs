14aa80eb89600ad1dfa0dd3db36c95235750a5d1
ceetek1 <charles.chukwurah@pau.edu.ng>
2025-12-09 01:57:18 +0100

    added a loop to this

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -27,14 +27,15 @@ fn what_is_your_name() -> String{
 }
 
 fn main(){
-    println!("Hello what is your name");
+    loop{
+        println!("Hello what is your name");
     let name = what_is_your_name();
     println!("Hello,{}", name);
     let visitors_list = ["charles","david","daniel","john","kamsi","zikora"];
     let mut allow_them_in = false;
-    for visitor in &visitors_list {
-        if visitor == &name {
-            allow_them_in = true;
+    for person in visitors_list {
+        if person == name {
+            allow_them_in = true;      // I didnt even need to reference anything
         }
     }
     if allow_them_in {
@@ -43,9 +44,27 @@ fn main(){
     else {
         println!("SORRY YOU ARE NOT ON MY LIST BITCH !!!");
     }
+    
     let mut file = std::fs::File::create("visitors allowed in.txt").expect("failed to create file");
     file.write_all(visitors_list.join("\n").as_bytes()).unwrap();
-    println!("file printed to screen"); 
+    println!("file printed to screen"); //sucess it worked 
 
+
+    let mut exit = String::new();
+    println!("Exit (YES or NO");
+    stdin().read_line(&mut exit).expect("failed to read line");
+    let exit = exit.trim().to_uppercase();
+
+    if exit == "YES"{
+        println!("GOOD BYE!!");
+        break;
+    }
+    if exit == "NO"{
+        println!("OK👍");
+        continue;
+    }
+
+    
+    }
     
 }
